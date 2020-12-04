9226e83ab4cf31ee7f411ad871bc4f9ebb8297b8
srgooglo <chippotato39@gmail.com>
2020-12-04 16:51:50 +0100

    avoid send request if not necesary

--- src/lib/flushNickname.js
+++ src/lib/flushNickname.js
@@ -2,6 +2,9 @@ import runtime from '@nodecorejs/dot-runtime'
 
 function flush() {
     if (runtime.ryo.originName) {
+        if (global.self.nickname == runtime.ryo.originName) {
+            return false
+        }
         global.self.setNickname(`${runtime.ryo.originName}`)
     }
 }
